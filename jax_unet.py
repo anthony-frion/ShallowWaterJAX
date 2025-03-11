615b6b7d89aa521c34a35e8728545094e3332376
alicanbekar <acbekar@arizona.edu>
2025-03-11 10:23:15 +0100

    coded minhs doubleconv on jax

--- jax_unet.py
+++ jax_unet.py
@@ -1,56 +1,68 @@
-import flax.linen as nn
+from flax import nnx
 import jax
 import jax.numpy as jnp
 
-class unetConv(nn.Module): 
-    out_dim: int
 
-    #Conv Parameters
-    kernel_size: tuple = (3, 3)
-    strides: int = 1
-    padding: int = 0
+class DoubleConv2D(nnx.Module):
+    def __init__(
+        self, in_channels, out_channels, kernel_size, strides, padding, num_groups
+    ):
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.num_groups = num_groups
+        self.conv1 = nnx.Conv(
+            in_features=self.in_channels,
+            out_features=self.out_channels,
+            kernel_size=self.kernel_size,
+            strides=self.strides,
+            padding=self.padding,
+        )
+        self.conv2 = nnx.Conv(
+            in_features=self.in_channels,
+            out_features=self.out_channels,
+            kernel_size=self.kernel_size,
+            strides=self.strides,
+            padding=self.padding,
+        )
+        self.norm1 = nnx.GroupNorm(
+            num_features=self.in_channels, num_groups=self.num_groups
+        )
+        self.norm2 = nnx.GroupNorm(
+            num_features=self.in_channels, num_groups=self.num_groups
+        )
 
-    use_batchnorm: bool = False
-
-    # BatchNorm Parameters
-    use_running_average: bool = False
-    momentum: float = 0.9
-    epsilon: float = 1e-5
-    dtype: jnp.dtype = jnp.float32
-    
-    @nn.compact
     def __call__(self, x):
-        if self.use_batchnorm:
-            x = nn.Conv(features = self.out_dim, kernel_size = self.kernel_size, strides = self.strides, padding = self.padding)(x) 
-            x = nn.BatchNorm(use_running_average = self.use_running_average, momentum = self.momentum, epsilon = self.epsilon, dtype = self.dtype)(x) 
-            x = nn.relu(x)
-            x = nn.Conv(features = self.out_size, kernel_size = self.kernel_size, strides = self.strides, padding= self.padding)(x)
-            x = nn.BatchNorm(use_running_average = self.use_running_average, momentum = self.momentum, epsilon = self.epsilon, dtype = self.dtype)(x)
-            x = nn.relu(x)
-            return x
-        else:
-            x = nn.Conv(features = self.out_dim, kernel_size = self.kernel_size, strides = self.strides, padding = self.padding)(x)
-            x = nn.relu(x)
-            x = nn.Conv(features = self.out_dim, kernel_size = self.kernel_size, strides = self.strides, padding = self.padding)(x) 
-            x = nn.relu(x)
-            return x
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = nnx.silu(x)
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = nnx.silu(x)
+        return x
 
 
-class Upsample(nn.Module):  
+class Upsample(nnx.Module):
     out_dim: int
     is_deconv: bool
-    
+
     # ConvTranspose Parameters
     kernel_size: int = 2
     strides: int = 2
 
     @nn.compact
     def forward(self, inputs1, inputs2):
-        
+
         if self.is_deconv:
-            outputs2 = nn.ConvTranspose(features = self.out_dim, kernel_size = self.kernel_size, strides = self.strides)(inputs2)
+            outputs2 = nn.ConvTranspose(
+                features=self.out_dim,
+                kernel_size=self.kernel_size,
+                strides=self.strides,
+            )(inputs2)
         else:
-            outputs2 = nn.UpsamplingBilinear2d(scale_factor=2) (inputs2)
+            outputs2 = nn.UpsamplingBilinear2d(scale_factor=2)(inputs2)
 
         offset = outputs2.size()[2] - inputs1.size()[2]
 
@@ -58,12 +70,15 @@ class Upsample(nn.Module):
 
         outputs1 = jnp.pad(inputs1, padding)
 
-        return unetConv(features = self.out_dim, is_batchnorm = False) (jnp.concatenate([outputs1, outputs2], 1))
+        return unetConv(features=self.out_dim, is_batchnorm=False)(
+            jnp.concatenate([outputs1, outputs2], 1)
+        )
+
 
 class unet(nn.Module):
-    feature_scale: int =4 
-    n_classes: int = 21 
-    is_deconv: bool = True  
+    feature_scale: int = 4
+    n_classes: int = 21
+    is_deconv: bool = True
     use_batchnorm: bool = True
     kernel_size: int = 2
 
@@ -80,29 +95,30 @@ class unet(nn.Module):
         # downsampling
 
         conv1 = unetConv(filters[0], use_batchnorm)(x)
-        maxpool1 = nn.MaxPool2d(kernel_size = self.kernel_size)(conv1)
+        maxpool1 = nn.MaxPool2d(kernel_size=self.kernel_size)(conv1)
 
         conv2 = unetConv(filters[1], use_batchnorm)(maxpool1)
-        maxpool2 = nn.MaxPool2d(kernel_size = self.kernel_size)(conv2)
+        maxpool2 = nn.MaxPool2d(kernel_size=self.kernel_size)(conv2)
 
         conv3 = unetConv(filters[2], use_batchnorm)(maxpool2)
-        maxpool3 = nn.MaxPool2d(kernel_size = self.kernel_size)(conv3)
+        maxpool3 = nn.MaxPool2d(kernel_size=self.kernel_size)(conv3)
 
         conv4 = unetConv(filters[3], use_batchnorm)(maxpool3)
-        maxpool4 = nn.MaxPool2d(kernel_size = self.kernel_size)(conv4)
+        maxpool4 = nn.MaxPool2d(kernel_size=self.kernel_size)(conv4)
 
         center = unetConv(filters[4], use_batchnorm)(maxpool4)
 
         # upsampling
-        up4 = Upsample(filters[3], is_deconv = is_deconv)(conv4, center)
-        up3 = Upsample(filters[2], is_deconv = is_deconv)(conv3, up4)
-        up2 =  Upsample(filters[1], is_deconv = is_deconv)(conv2, up3)
-        up1 = Upsample(filters[0], is_deconv = is_deconv)(conv1, up2)
+        up4 = Upsample(filters[3], is_deconv=is_deconv)(conv4, center)
+        up3 = Upsample(filters[2], is_deconv=is_deconv)(conv3, up4)
+        up2 = Upsample(filters[1], is_deconv=is_deconv)(conv2, up3)
+        up1 = Upsample(filters[0], is_deconv=is_deconv)(conv1, up2)
 
         # final conv (without any concat)
         final = nn.Conv(self.n_classes, 1)(up1)
 
         return final
+
 
 if __name__ == "__main__":
     pass
