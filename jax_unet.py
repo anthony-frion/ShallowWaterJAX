d277ed978b386e3a02fef03d129f2f91ec2bd0fd
Minh Nguyen <ntvminh286@gmail.com>
2025-03-12 17:16:20 +0100

    prediction for jax Unet2d

--- jax_unet.py
+++ jax_unet.py
@@ -171,10 +171,6 @@ class unet(nnx.Module):
         self.stride = stride
         self.padding = padding
         rngs = nnx.Rngs(0)
-<<<<<<< HEAD
-        self.dynamics = nnx.Conv(in_features=in_channels[0], out_features=int(features[0]/2), kernel_size=kernel_size, strides=stride, padding=padding, rngs=rngs)
-        self.statics = nnx.Conv(in_features=in_channels[1], out_features=int(features[0]/2), kernel_size=kernel_size, strides=stride, padding=padding, rngs=rngs)
-=======
         self.dynamics = nnx.Conv(
             in_features=in_channels[0],
             out_features=int(features[0] / 2),
@@ -191,16 +187,11 @@ class unet(nnx.Module):
             padding=padding,
             rngs=rngs,
         )
->>>>>>> working-unet
 
         self.encoder = []
         for i in range(num_layers):
             in_ch = features[i - 1] if i > 0 else features[0]
             out_ch = features[i]
-<<<<<<< HEAD
-            self.encoder.append(DoubleConv2D(in_channels=in_ch, out_channels=out_ch, kernel_size=kernel_size, strides=stride, padding=padding, num_groups=out_ch//4))
-        
-=======
             self.encoder.append(
                 DoubleConv2D(
                     in_channels=in_ch,
@@ -212,30 +203,9 @@ class unet(nnx.Module):
                 )
             )
 
->>>>>>> working-unet
         self.up = []
         for i in range(num_layers - 1, 0, -1):
             in_ch = features[i]
-<<<<<<< HEAD
-            out_ch = features[i-1]
-            self.up.append(nnx.ConvTranspose(in_features=in_ch, out_features=out_ch, kernel_size=(2, 2), strides=2, rngs=rngs))
-            
-        self.decoder = []
-        for i in range(num_layers-1, 0, -1):
-            in_ch = features[i]
-            out_ch = features[i-1]
-            self.decoder.append(DoubleConv2D(in_channels=in_ch, out_channels=out_ch, kernel_size=kernel_size, strides=stride, padding=padding, num_groups=out_ch//4)) 
-        
-        self.output = []
-        for i in range(out_channels):
-            self.output.append(nnx.Conv(in_features=features[0], out_features=1, kernel_size=1, strides=1, padding=0, rngs=rngs))  
-        
-    def __call__(self, x, **kwargs):
-        H = kwargs['H']
-        mask = kwargs['mask']
-        x_dynamics = self.dynamics(x)
-        x_statics = self.statics(jnp.concatenate([H, mask], axis=-1)) # I guess we need to change it to axis=-1 because flax has channel dim last.
-=======
             out_ch = features[i - 1]
             self.up.append(
                 nnx.ConvTranspose(
@@ -366,7 +336,6 @@ class unet(nnx.Module):
         x_statics = self.statics(
             jnp.concatenate([H, mask], axis=-1)
         )  # I guess we need to change it to axis=-1 because flax has channel dim last.
->>>>>>> working-unet
         x = jnp.concatenate([x_dynamics, x_statics], axis=-1)
 
         skip_connections = []
@@ -374,15 +343,6 @@ class unet(nnx.Module):
             if i == 0:
                 x = self.encoder[i](x)
             else:
-<<<<<<< HEAD
-                x = self.encoder[i](linen.max_pool(x, window_shape=(2, 2), strides=(2, 2)))
-            skip_connections.append(x)
-        skip_connections = skip_connections[::-1][1:] # reverse the list and remove the first element
-        for i in range(self.num_layers-1):
-            x = self.up[i](x)
-            if x.shape[1] != skip_connections[i].shape[1] or x.shape[2] != skip_connections[i].shape[2]:
-                diffY = skip_connections[i].shape[1]- x.shape[1]
-=======
                 x = self.encoder[i](
                     linen.max_pool(x, window_shape=(2, 2), strides=(2, 2))
                 )
@@ -397,23 +357,11 @@ class unet(nnx.Module):
                 or x.shape[2] != skip_connections[i].shape[2]
             ):
                 diffY = skip_connections[i].shape[1] - x.shape[1]
->>>>>>> working-unet
                 diffX = skip_connections[i].shape[2] - x.shape[2]
                 pad_left = diffX // 2
                 pad_right = diffX - pad_left
                 pad_top = diffY // 2
                 pad_bottom = diffY - pad_top
-<<<<<<< HEAD
-                x = jnp.pad(x, ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right), (0, 0)), mode='constant')
-            x = jnp.concatenate([x, skip_connections[i]], axis=-1)
-            x = self.decoder[i](x)
-            
-        # Output part remains the same from original code
-        outputs = []
-        for i in range(self.out_channels):
-            outputs.append(self.output[i](x))
-        return jnp.concatenate(outputs, axis=-1)
-=======
                 x = jnp.pad(
                     x,
                     ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right), (0, 0)),
@@ -440,7 +388,7 @@ class unet(nnx.Module):
             "num_layers": 5,
         }
         model = UNET2D(**network_params)
-        filename = "model.pth.tar"
+        filename = "data/model.pth.tar"
         model = load_model(model, filename).to(DEVICE)
         print(model)
         checkpoint = get_checkpoint(filename)
@@ -507,7 +455,6 @@ class unet(nnx.Module):
                 )
 
         return self
->>>>>>> working-unet
 
 
 # Test code
@@ -520,11 +467,6 @@ def test_unet():
     kernel_size = (3, 3)
     stride = 1
     padding = 1
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> working-unet
     # Create model
     model = unet(
         num_layers=num_layers,
@@ -533,36 +475,15 @@ def test_unet():
         features=features,
         kernel_size=kernel_size,
         stride=stride,
-<<<<<<< HEAD
-        padding=padding
-    )
-    
-=======
         padding=padding,
     )
 
->>>>>>> working-unet
     # Create sample input data
     batch_size = 1
     height, width = 100, 100
     x = jnp.ones((1, height, width, in_channels[0]))  # Dynamic input
     H = jnp.ones((1, height, width, 1))  # Static terrain
     mask = jnp.ones((1, height, width, 1))  # Binary mask
-<<<<<<< HEAD
-    
-    # Pass data through model
-    output = model(x, H=H, mask=mask)
-    
-    print(f"Input shape: {x.shape}")
-    print(f"Output shape: {output.shape}")
-    print(f"Expected output channels: {out_channels}")
-    
-    return output
-
-# Run the test
-if __name__ == "__main__":
-    test_unet()
-=======
 
     # Pass data through model
     output = model(x, H=H, mask=mask)
@@ -580,4 +501,3 @@ if __name__ == "__main__":
 # Run the test
 if __name__ == "__main__":
     test_unet()
->>>>>>> working-unet
